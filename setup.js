3e4e3b928a03893037e70b8ec6eb0a368340ea6e
Steve Lacy <me@slacy.me>
2015-01-15 20:37:44 -0700

    add prompter

---
+++ setup.js
@@ -0,0 +1,114 @@
+var fs = require('fs');
+var prompt = require('prompt');
+var template = require('lodash.template');
+var merge = require('lodash.merge');
+var rmraf = require('rimraf');
+
+var argv = process.argv;
+
+var defaults = {
+  version: '0.0.1',
+  yourName: 'We Are Fractal',
+  yourEmail: 'contact@wearefractal.com',
+  yourDomain: 'http://wearefractal.com',
+  gitUserName: 'wearefractal',
+  main: './index.js',
+  test: 'npm run-script lint && mocha --reporter spec',
+  lint: 'jshint . --exclude node_modules --config .jshintrc'
+};
+
+
+var questions = {
+  properties: {
+    projectName: {
+      pattern: /^[a-zA-Z\-]+$/,
+      message: 'name must not contain spaces',
+      required: true
+    },
+    description: {},
+    version: {
+      default: defaults.version
+    },
+    yourName: {
+      required: true,
+      default: defaults.yourName
+    },
+    yourEmail: {
+      required: true,
+      default: defaults.yourEmail
+    },
+    yourDomain: {
+      required: true,
+      default: defaults.yourDomain
+    },
+    gitUserName: {
+      required: true,
+      default: defaults.gitUserName
+    },
+    main: {
+      default: defaults.main
+    },
+    test: {
+      default: defaults.test
+    },
+    lint: {
+      default: defaults.lint
+    }
+  }
+};
+
+prompt.start();
+
+if (argv[2] == '--use-defaults') {
+  console.log('Using defaults \n');
+
+  defaultQuestions = {
+    properties: {
+      projectName: {
+        pattern: /^[a-zA-Z\-]+$/,
+        message: 'name must not contain spaces',
+        required: true
+      },
+      description: {},
+    }
+  };
+
+  return prompt.get(defaultQuestions, function(err, data) {
+    data = merge(data, defaults);
+    save(data);
+  });
+}
+
+prompt.get(questions, function(err, data) {
+  save(data);
+});
+
+
+function save(data) {
+  if (data === null || data === undefined) {
+    return;
+  }
+  data.year = new Date().getFullYear();
+  var pkg = './package.json';
+  var readme = './README.md';
+  var license = './LICENSE';
+
+  fs.rename(pkg + '.bk', pkg, function() {
+    fs.writeFileSync(pkg, template(fs.readFileSync(pkg), data));
+
+    fs.rename(readme + '.bk', readme, function() {
+      fs.writeFileSync(readme, template(fs.readFileSync(readme), data));
+      fs.writeFileSync(license, template(fs.readFileSync(license), data));
+
+      rmraf('node_modules', function (err) {
+        if (err) return console.log(err);
+
+        rmraf('.git', function(err) {
+          if (err) return console.log(err);
+          console.log('Completed successfully\n');
+        });
+      });
+    });
+  });
+
+}
